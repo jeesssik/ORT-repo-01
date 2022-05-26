11dc1fa87d87d87f4ef2cb732b2ff94f31daeff7
jeesssik <jeesssik@gmail.com>
2022-05-25 23:02:15 -0300

    Ejemplo Node + MongoDB

---
+++ dai/NODE/NodeMongoDB/app.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  Entry = require("./api/models/leaderboardModel");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(
+  "mongodb+srv://jess:asd@cluster0.mxdd7.mongodb.net/?retryWrites=true&w=majority",
+  
+  err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB!!!')
+});
+
+
+var routes = require("./api/routes/leaderboardRoutes");
+routes(app);
+
+app.listen(port);
+
+console.log("API server started on " + port);
